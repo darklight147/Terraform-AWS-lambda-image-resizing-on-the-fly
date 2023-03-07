e725b4341f078a73ba176f5db3a7e7c8118dc361
Mohamed Belkamel <belkamelmohamed@gmail.com>
2023-03-07 20:23:09 +0100

    feat: added desploy & destroy scripts

--- deploy.sh
+++ deploy.sh
@@ -13,9 +13,9 @@ cd staging
 terraform init
 
 # check if variables.tfvars exists
-if [ ! -f variables.tfvars ]; then
-    echo "variables.tfvars does not exist. Please create it in the directory infra/staging, Exiting..."
+if [ ! -f terraform.tfvars ]; then
+    echo "terraform.tfvars does not exist. Please create it in the directory infra/staging, Exiting..."
     exit 1
 fi
 
-terraform apply -var-file=variables.tfvars
+terraform apply -auto-approve -input=false
--- destroy.sh
+++ destroy.sh
@@ -27,8 +27,8 @@ then
     terraform init
 
     # check if variables.tfvars exists
-    if [ ! -f variables.tfvars ]; then
-        echo "variables.tfvars does not exist. Please create it in the directory infra/staging, Exiting..."
+    if [ ! -f terraform.tfvars ]; then
+        echo "terraform.tfvars does not exist. Please create it in the directory infra/staging, Exiting..."
         exit 1
     fi
 
@@ -44,7 +44,7 @@ then
     if [[ $REPLY =~ ^[Yy]$ ]]
     then
         aws s3 rm s3://$S3_BUCKET --recursive --profile $(test -z $AWS_PROFILE && echo "default" || echo $AWS_PROFILE)
-        terraform destroy -var-file=variables.tfvars -input=false -auto-approve
+        terraform destroy -input=false -auto-approve
     fi
     else
         echo "Exiting..."
