4df708f75f054ef9748b7467dc406c39d72cbf18
Mohamed Belkamel <belkamelmohamed@gmail.com>
2023-03-09 10:59:12 +0100

    typo: fi

--- destroy.sh
+++ destroy.sh
@@ -14,8 +14,7 @@ read -p "what is your AWS profile? " AWS_PROFILE
 
 echo "AWS_PROFILE is now set to: $AWS_PROFILE"
 
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+if [[ $REPLY =~ ^[Yy]$ ]]; then
     cd lambda-resizer
 
     sam build
@@ -41,17 +40,14 @@ then
 
     echo
 
-    if [[ $REPLY =~ ^[Yy]$ ]]
-    then
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
         aws s3 rm s3://$S3_BUCKET --recursive --profile $(test -z $AWS_PROFILE && echo "default" || echo $AWS_PROFILE)
         terraform destroy -input=false -auto-approve
-    fi
     else
         echo "Exiting..."
         echo "Failed to destroy infrastructure"
         echo "Failed to delete S3 bucket $S3_BUCKET"
         exit 1
     fi
-
 
 fi
